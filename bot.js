e4c1598ee3a510eb1d1dfbd2580e05e72c2b967b
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-07 22:01:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,6 +67,47 @@ client.on('message', msg => {
 client.login(process.env.BOT_TOKEN);
 
 
+client.on('message', message => {
+    if (message.content.startsWith("!avatar")) {
+        var mentionned = message.mentions.users.first(); //by  BlackOps :D
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '!help') {
+    msg.reply('mmkn 7drtk tklm 7d mn el STAFF fe el Gourp 4rkan Welcome Pro!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
